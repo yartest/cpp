d568ab49a82563bd7979ccf24f5237c594741898
yartest <povezlo@gmail.com>
2019-07-06 19:02:23 +0300

    T

--- my_rvalue_lvalue.cpp
+++ my_rvalue_lvalue.cpp
@@ -23,6 +23,8 @@ int get_7() { return 7; }
 void my_rvalue_lvalue_01() {
 	int &&test = 10;
 	cout << "test:" << test << "\n";
+	print_value(99);
 	print_value(test);
+	print_value(move(test));
 	print_value(get_7());
 }
